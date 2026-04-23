016d94714b924aaf6b318516a5186f9fe48c8946
Tettano <shawnmarkr@gmail.com>
2026-04-23 17:22:01 +0800

    added depends on the root main.tf & modules/rds/main.tf

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -1,17 +1,17 @@
 resource "aws_db_subnet_group" "rds" {
   name       = "rds-subnet-group"
   subnet_ids = var.private_subnet_ids
-
-  tags = merge(var.common_tags, { Name = "RDS Subnet Group" })
+  depends_on = [aws_db_instance.myrds]
+  tags       = merge(var.common_tags, { Name = "RDS Subnet Group" })
 }
 
 resource "aws_db_parameter_group" "rds" {
   for_each = var.rds
 
-  family = each.value.parameter_group_family
-  name   = "rds-param-group-${each.key}"
-
-  tags = merge(var.common_tags, { Name = "RDS Parameter Group - ${each.key}" })
+  family     = each.value.parameter_group_family
+  name       = "rds-param-group-${each.key}"
+  depends_on = [aws_db_instance.myrds]
+  tags       = merge(var.common_tags, { Name = "RDS Parameter Group - ${each.key}" })
 }
 
 resource "aws_db_instance" "myrds" {
@@ -24,12 +24,12 @@ resource "aws_db_instance" "myrds" {
   instance_class         = each.value.instance_class
   username               = var.db_credentials.username
   password               = var.db_credentials.password
-  parameter_group_name   = aws_db_parameter_group.rds[each.key].name
   skip_final_snapshot    = each.value.skip_final_snapshot
+  db_subnet_group_name   = aws_db_subnet_group.rds.name
+  parameter_group_name   = aws_db_parameter_group.rds[each.key].name
   multi_az               = each.value.multi_az
   vpc_security_group_ids = var.vpc_security_group_ids
   storage_encrypted      = each.value.storage_encrypted
-  db_subnet_group_name   = aws_db_subnet_group.rds.name
   storage_type           = each.value.storage_type
 
   tags = merge(var.common_tags, { Name = "RDS Instance - ${each.key}" })
