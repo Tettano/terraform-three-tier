b13fc86a617ecdd9ba8e8c1774ad3b45f422a8b5
Tettano <shawnmarkr@gmail.com>
2026-04-24 15:34:39 +0800

    fix: format terraform files for CI check

--- main.tf
+++ main.tf
@@ -6,46 +6,46 @@ module "vpc" {
 }
 
 module "sg" {
-  source              = "./modules/sg"
-  vpc_id              = module.vpc.vpc_id
- 
+  source = "./modules/sg"
+  vpc_id = module.vpc.vpc_id
+
 }
 
 module "igw" {
-  source              = "./modules/igw"
-  vpc_id              = module.vpc.vpc_id
-  depends_on          = [module.nat, module.alb, module.ec2-asg]
+  source     = "./modules/igw"
+  vpc_id     = module.vpc.vpc_id
+  depends_on = [module.nat, module.alb, module.ec2-asg]
 }
 
 module "nat" {
-  source              = "./modules/nat"
-  public_subnet_ids   = module.vpc.public_subnet_ids
-  public_subnet_tags  = module.vpc.vpc_public_tags
+  source             = "./modules/nat"
+  public_subnet_ids  = module.vpc.public_subnet_ids
+  public_subnet_tags = module.vpc.vpc_public_tags
 
-  
+
 }
 
 module "rtb" {
-  source              = "./modules/rtb"
-  vpc_id              = module.vpc.vpc_id
-  igw_id              = module.igw.internet-gateway-id
-  nat_gateway_ids     = module.nat.nat_gateway_ids
-  public_subnet_ids   = module.vpc.public_subnet_ids
-  private_subnet_ids  = module.vpc.private_subnet_ids
+  source             = "./modules/rtb"
+  vpc_id             = module.vpc.vpc_id
+  igw_id             = module.igw.internet-gateway-id
+  nat_gateway_ids    = module.nat.nat_gateway_ids
+  public_subnet_ids  = module.vpc.public_subnet_ids
+  private_subnet_ids = module.vpc.private_subnet_ids
 
 }
 
 module "alb" {
-  source              = "./modules/alb"
-  vpc_id              = module.vpc.vpc_id
-  subnet_ids          = module.vpc.public_subnet_ids
-  alb_sg_id           = module.sg.alb_sg_id
+  source     = "./modules/alb"
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.public_subnet_ids
+  alb_sg_id  = module.sg.alb_sg_id
 
 }
 
 module "ec2-asg" {
-  source              = "./modules/ec2-asg"
-  subnet_ids          = module.vpc.private_subnet_ids
+  source     = "./modules/ec2-asg"
+  subnet_ids = module.vpc.private_subnet_ids
 }
 
 module "rds" {
