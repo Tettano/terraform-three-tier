f55b206196d9540c4a07ede990cf8967102ef765
Tettano <shawnmarkr@gmail.com>
2026-04-24 15:59:17 +0800

    update the igw module

--- modules/igw/main.tf
+++ modules/igw/main.tf
@@ -5,4 +5,9 @@ resource "aws_internet_gateway" "igw" {
 resource "aws_internet_gateway_attachment" "igw_attachment" {
   vpc_id              = var.vpc_id
   internet_gateway_id = aws_internet_gateway.igw.id
-}   
+}
+
+resource "time_sleep" "wait_for_igw_propagation" {
+  create_duration = "10s"
+  depends_on      = [aws_internet_gateway_attachment.igw_attachment]
+}
